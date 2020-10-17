dd0df77f1c2e8222e87cde1dd55ed06252fb04c4
Zijian Liu <liu.zijian@outlook.com>
2020-10-17 07:15:14 -0400

    fix

--- src/spotify_client_config.jsx
+++ src/spotify_client_config.jsx
@@ -1,7 +1,7 @@
 const spotify_client = {
     client_id: 'c86395f9e10f46789da8c175dd76eea5',
     client_secret: '58e139c6808244518abbeb8bc1aed95e',
-    redirect_uri: window.location.href.indexOf('localhost') ?
+    redirect_uri: window.location.href.indexOf('localhost') !== -1 ?
         'http://localhost:3000/' :
         'https://graysonliu.github.io/spotify-charts-generator/'
 }

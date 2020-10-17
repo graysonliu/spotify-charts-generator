3d484bb5494a676e30081ae3a398c3b99e26da51
Zijian Liu <liu.zijian@outlook.com>
2020-10-17 07:10:10 -0400

    popup auth

--- src/App.jsx
+++ src/App.jsx
@@ -10,46 +10,55 @@ const client_id = spotify_client.client_id
 const client_secret = spotify_client.client_secret
 const redirect_uri = spotify_client.redirect_uri
 
-console.log(client_id, client_secret)
-
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {access_token: null, refresh_token: null}
+        this.state = {access_token: null, refresh_token: null};
+        this.popup = null;
+        window.spotifyAuthCallback = this.spotifyAuthCallback.bind(this);
+    }
+
+    spotifyAuthCallback(code) {
+        // close popup
+        this.popup.close();
+        // use code to request token
+        $.ajax({
+            method: "POST",
+            url: "https://accounts.spotify.com/api/token",
+            data: {
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: redirect_uri,
+                client_id: client_id,
+                client_secret: client_secret,
+            }
+        }).done(data => {
+            console.log(data)
+            this.setState({
+                access_token: data['access_token'],
+                refresh_token: data['refresh_token'],
+            });
+        });
     }
 
     render() {
+        const queries = processURL(window.location.href)
         // spotify authentication
-        if (!this.state.access_token && !('code' in processURL(window.location.href))) {
-            window.location.href = 'https://accounts.spotify.com/authorize' +
+        if (!this.state.access_token && !('code' in queries)) {
+            // not authenticated yet
+            // this is the main window
+            this.popup = window.open('https://accounts.spotify.com/authorize' +
                 '?response_type=code' +
                 '&client_id=' + client_id +
                 (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
                 '&redirect_uri=' + encodeURIComponent(redirect_uri) +
-                '&show_dialog=' + true;
-        }
-
-        const queries = processURL(window.location.href)
-
-        // use code to request token
-        if (!this.state.access_token && 'code' in queries) {
-            $.ajax({
-                method: "POST",
-                url: "https://accounts.spotify.com/api/token",
-                data: {
-                    grant_type: 'authorization_code',
-                    code: queries['code'],
-                    redirect_uri: redirect_uri,
-                    client_id: client_id,
-                    client_secret: client_secret,
-                }
-            }).done(data => {
-                console.log(data)
-                this.setState({
-                    access_token: data['access_token'],
-                    refresh_token: data['refresh_token'],
-                });
-            }).fail(() => window.location.href = redirect_uri);
+                '&show_dialog=' + true,
+                'Login with Spotify',
+                'width=800,height=600');
+        } else if ('code' in queries) {
+            // authenticated
+            // this is a popup window, window.opener is the main window
+            window.opener.spotifyAuthCallback(queries['code']);
         }
 
         return (

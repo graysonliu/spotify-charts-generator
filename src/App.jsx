6ee62c338c0c5988ac8b9b45d880b7131b875933
Zijian Liu <liu.zijian@outlook.com>
2020-10-18 07:52:09 -0400

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,79 @@
 import React, {Component} from "react";
 import $ from "jquery"
 import processURL from "./process_url";
+import logo_thinking from "./images/thinking.svg"
+import "./styles.scss"
+
+class ImageLink extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='image-link'>
+                <a href={this.props.href || '/'}
+                   target={this.props.target || '_blank'}>
+                    <img src={this.props.img}
+                         alt={this.props.alt || ''}
+                         className={this.props.rotating && 'rotating-img'}
+                         onMouseOver={this.props.onMouseOverImage}
+                         onMouseLeave={this.props.onMouseLeaveImage}/>
+                </a>
+            </div>);
+    }
+}
+
+class MyLogo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='my-logo'>
+                <ImageLink
+                    target='_self'
+                    img={logo_thinking}
+                    rotating={true}
+                />
+            </div>);
+    }
+
+}
+
+class LoginButton extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.props.onClick}>Login</button>
+            </div>
+        );
+    }
+
+}
 
 class SpotifyApp extends Component {
     constructor(props) {
         super(props);
         this.state = {
             access_token: null,
-            refresh_token: null,
-            redirect_uri: this.props.redirect_uri
+            refresh_token: null
         };
         this.popup = null;
-        window.spotifyAuthCallback = this.spotifyAuthCallback.bind(this);
+        window.spotifyAuthSuccessCallback = this.spotifyAuthSuccessCallback.bind(this);
+        window.spotifyAuthCanceledCallback = this.spotifyAuthCanceledCallback.bind(this);
+        this.handleClickLoginButton = this.handleClickLoginButton.bind(this);
     }
 
-    spotifyAuthCallback(code) {
-        // close popup
+    spotifyAuthSuccessCallback(code) {
+        // close the popup window
         this.popup.close();
+
         // use code to request token
         $.ajax({
             method: "POST",
@@ -29,7 +86,6 @@ class SpotifyApp extends Component {
                 client_secret: this.props.client_secret,
             }
         }).done(data => {
-            console.log(data)
             this.setState({
                 access_token: data['access_token'],
                 refresh_token: data['refresh_token'],
@@ -37,29 +93,53 @@ class SpotifyApp extends Component {
         });
     }
 
+    handleClickLoginButton(e) {
+        this.popup = window.open('https://accounts.spotify.com/authorize' +
+            '?response_type=code' +
+            '&client_id=' + this.props.client_id +
+            (this.props.scopes ? '&scope=' + encodeURIComponent(this.props.scopes) : '') +
+            '&redirect_uri=' + encodeURIComponent(this.props.redirect_uri) +
+            '&show_dialog=' + true,
+            'Login with Spotify');
+    }
+
+    spotifyAuthCanceledCallback() {
+        // close the popup window
+        this.popup.close();
+    }
+
     render() {
         const queries = processURL(window.location.href)
         // spotify authentication
-        if (!this.state.access_token && !('code' in queries)) {
-            // not authenticated yet
-            // this is the main window
-            this.popup = window.open('https://accounts.spotify.com/authorize' +
-                '?response_type=code' +
-                '&client_id=' + this.props.client_id +
-                (this.props.scopes ? '&scope=' + encodeURIComponent(this.props.scopes) : '') +
-                '&redirect_uri=' + encodeURIComponent(this.props.redirect_uri) +
-                '&show_dialog=' + true,
-                'Login with Spotify',
-                'width=800,height=600');
-        } else if ('code' in queries) {
-            // authenticated
-            // this is a popup window, window.opener is the main window
-            window.opener.spotifyAuthCallback(queries['code']);
+        if (!this.state.access_token) {
+            if (!('code' in queries) && !('error' in queries)) {
+                // not authenticated yet
+                // this is the main window
+                return (
+                    <div className='app'>
+                        <MyLogo/>
+                        <LoginButton onClick={this.handleClickLoginButton}/>
+                    </div>);
+            }
+            if ('code' in queries) {
+                // authenticated and this is a popup window
+                // window.opener is the main window
+                window.opener && window.opener.spotifyAuthSuccessCallback(queries['code']);
+                return (<div className='app'/>);
+            }
+            if ('error' in queries) {
+                // authentication canceled and this is a popup window
+                // window.opener is the main window
+                window.opener && window.opener.spotifyAuthCanceledCallback();
+                return (<div className='app'/>);
+            }
+        } else {
+            return (
+                <div className='app'>
+                    <MyLogo/>
+                </div>
+            );
         }
-
-        return (
-            <div>
-            </div>);
     }
 }
 
--- src/process_url.jsx
+++ src/process_url.jsx
@@ -1,7 +1,7 @@
 // https://example.com/callback?code=...&state=...
 const processURL = url => {
     const queries = {}
-    url.substring(url.indexOf('?') + 1)
+    url.indexOf('?') !== -1 && url.substring(url.indexOf('?') + 1)
         .split('&')
         .map(queryString => {
             const [key, val] = queryString.split('=');
